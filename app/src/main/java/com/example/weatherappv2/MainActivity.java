4fc83d02c4f6cb8b72b2e96b355c050d71e02d21
Lev <levparygin3@gmail.com>
2023-04-14 22:09:52 +0600

    5daysweather update

--- app/src/main/java/com/example/weatherappv2/MainActivity.java
+++ app/src/main/java/com/example/weatherappv2/MainActivity.java
@@ -7,6 +7,7 @@ import android.annotation.SuppressLint;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -22,7 +23,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.Inet4Address;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -31,7 +31,8 @@ public class MainActivity extends AppCompatActivity {
     private TextInputEditText editCity;
     private ImageFilterView search;
     private ImageView weatherIcon;
-    private TextView temperature,cityName, condition,feelsLike,humidity,wind,pressure,max_temp;
+    private TextView temperature,cityName, condition,feelsLike,humidity,wind,pressure,max_temp,temp2day,temp3day,temp4day,temp5day,day3date,day4date,day5date,avgCityName;
+    private Button next4day;
 
 
     @SuppressLint({"WrongViewCast", "MissingInflatedId"})
@@ -52,6 +53,18 @@ public class MainActivity extends AppCompatActivity {
         pressure = findViewById(R.id.pressure_text);
         max_temp = findViewById(R.id.temp_max_text);
 
+        next4day = findViewById(R.id.button_week);
+        temp2day = findViewById(R.id.day2_temp);
+        temp3day = findViewById(R.id.day3_temp);
+        temp4day = findViewById(R.id.day4_temp);
+        temp5day = findViewById(R.id.day5_temp);
+        day3date = findViewById(R.id.day3);
+        day4date = findViewById(R.id.day4);
+        day5date = findViewById(R.id.day5);
+        avgCityName = findViewById(R.id.avg);
+
+
+
         search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -68,6 +81,18 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        next4day.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String city = editCity.getText().toString().trim();
+                avgCityName.setText("Average temp:"+city);
+                String key = "8520eae302d1fd79163dac50bbcd4ff1";
+                String url = "https://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid="+key+"&units=metric";
+                new GetURLData5days().execute(url);
+            }
+
+        });
     }
 
     @SuppressLint("StaticFieldLeak")
@@ -124,6 +149,7 @@ public class MainActivity extends AppCompatActivity {
                 temperature.setText( object.getJSONObject("main").getString("temp")+"°C");
                 feelsLike.setText("Feels like:"+object.getJSONObject("main").getString("feels_like")+"°C");
                 cityName.setText(editCity.getText().toString());
+
                 String cur_condition = object.getJSONArray("weather").getJSONObject(0).getString("main").toLowerCase();
                 condition.setText(cur_condition.toString());
                 Picasso.get().load(resolveImage(cur_condition)).into(weatherIcon);
@@ -166,4 +192,99 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+
+    private class GetURLData5days extends AsyncTask<String,String,String>{
+
+        @Override
+        protected String doInBackground(String... strings) {
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+
+            try {
+                URL url = new URL(strings[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuffer buffer = new StringBuffer();
+                String line = "";
+
+                while ((line = reader.readLine()) != null)
+                    buffer.append(line).append("\n");
+
+                return buffer.toString();
+
+            } catch (MalformedURLException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }finally {
+                if (connection!=null)
+                    connection.disconnect();
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }
+
+        @SuppressLint({"DefaultLocale", "SetTextI18n"})
+        @Override
+        protected void onPostExecute(String result){
+            super.onPostExecute(result);
+
+            try {
+                JSONObject jsonObject = new JSONObject(result);
+                double day2=0,day3 = 0,day4 = 0,day5 = 0;
+                int arr = jsonObject.getJSONArray("list").length();
+                for (int i = 8; i < arr; i++){
+                    if (i>=8 && i<=16)
+                        day2 += jsonObject.getJSONArray("list").getJSONObject(0).getJSONObject("main").getDouble("temp");
+                    else if (i>16 && i<=24) {
+                        day3 += jsonObject.getJSONArray("list").getJSONObject(0).getJSONObject("main").getDouble("temp");
+                    }
+                    else if (i>24 && i<=32) {
+                        day4 += jsonObject.getJSONArray("list").getJSONObject(0).getJSONObject("main").getDouble("temp");
+                    }
+                    else if (i>32 && i<=40) {
+                        day5 += jsonObject.getJSONArray("list").getJSONObject(0).getJSONObject("main").getDouble("temp");
+                    }
+                }
+
+                    String date1 = jsonObject.getJSONArray("list").getJSONObject(16).getString("dt_txt");
+                    String date2 = jsonObject.getJSONArray("list").getJSONObject(24).getString("dt_txt");
+                    String date3 = jsonObject.getJSONArray("list").getJSONObject(32).getString("dt_txt");
+
+                    day3date.setText(date1.substring(date1.indexOf("-")+1,date1.indexOf(" ")).substring(3) + "." + date1.substring(date1.indexOf("-")+1,date1.indexOf(" ")).substring(0, 2));
+                    day4date.setText(date2.substring(date2.indexOf("-")+1,date2.indexOf(" ")).substring(3) + "." + date2.substring(date2.indexOf("-")+1,date1.indexOf(" ")).substring(0, 2));
+                    day5date.setText(date3.substring(date3.indexOf("-")+1,date3.indexOf(" ")).substring(3) + "." + date3.substring(date1.indexOf("-")+1,date1.indexOf(" ")).substring(0, 2));
+
+
+
+
+
+
+
+
+
+                temp2day.setText(String.format("%.2f", day2 / 8));
+                temp3day.setText(String.format("%.2f", day3 / 8));
+                temp4day.setText(String.format("%.2f", day4 / 8));
+                temp5day.setText(String.format("%.2f", day5 / 8));
+
+
+
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+    }
+
+
 }
